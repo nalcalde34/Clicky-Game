33702b564694c2ab1ac33f7e07312aa23ca8da0a
Nico A <nalcalde034@gmail.com>
2019-10-06 22:07:31 -0400

    Updates

---
+++ precache-manifest.03e24dad0e0f73df0e16520d4208bdf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6cdf6ecb5d9430d25402716738f615",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4a706126e23083c91a89",
+    "url": "/Clicky-Game/static/css/main.dbebf10c.chunk.css"
+  },
+  {
+    "revision": "3587fe1c8c59f6461d68",
+    "url": "/Clicky-Game/static/js/2.a541c5b0.chunk.js"
+  },
+  {
+    "revision": "4a706126e23083c91a89",
+    "url": "/Clicky-Game/static/js/main.99f38b91.chunk.js"
+  },
+  {
+    "revision": "e13f71289a250d2bd938",
+    "url": "/Clicky-Game/static/js/runtime-main.413578af.js"
+  }
+]);
